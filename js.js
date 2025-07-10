158b217260667f881d12b229b1d51263f08a0773
¨Seva <sevakunjadas.bms@gmail.com>
2025-07-10 14:06:42 -0300

    ok

---
+++ js.js
@@ -0,0 +1,19 @@
+
+const colorButton = document.getElementById('colorButton');
+const body = document.body;
+
+
+const colors = ['#fabfb7', '#fdf9c4', '#ffda9e', '#c5c6c8',
+                '#b2e2f2', '#b0c2f2', '#ffe4e1'];
+
+
+function colorFondo() {
+    
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+    
+    body.style.backgroundColor = randomColor;
+    console.log("The new color is:", randomColor);
+}
+
+colorButton.addEventListener('click', colorFondo);
